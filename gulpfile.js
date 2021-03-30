ce835cefc68283b5683db4f9a88db16bebba0f85
alexisben <alexiben7@gmail.com>
2021-03-30 10:29:09 +0200

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -12,26 +12,8 @@ function html () {
 function css () {
   return gulp.src('_site/**/*.css')
     .pipe(prefix())
-    .pipe(cleanCSS({
-        compatibility: 'ie8'
-    }))
+    .pipe(cleanCSS())
     .pipe(gulp.dest('_site'));
 };
-
-// exports.default = function() {
-//     return gulp.src('_site/**/*.html')
-//         .pipe(htmlmin({ 
-//             collapseWhitespace: true
-//         }))
-//         .pipe(gulp.dest('_site'))
-//         .pipe(gulp.src('_site/**/*.css'))
-//         .pipe(prefix({
-//             browsers: ['last 2 versions']
-//         }))
-//         .pipe(cleanCSS({
-//             compatibility: 'ie8'
-//         }))
-//         .pipe(gulp.dest('_site'))
-// }
 
 exports.default = gulp.parallel(css, html);
